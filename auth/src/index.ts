000614df20e53bcc1b9d8dfd297a1db661257132
Anand-Velpuri <129038384+Anand-Velpuri@users.noreply.github.com>
2026-07-07 11:00:08 +0530

    Project Structure and Completed Auth service and tested locally with Skaffold and Kubernetes

---
+++ auth/src/index.ts
@@ -0,0 +1,51 @@
+import express from 'express';
+import 'express-async-errors';
+import { currentUserRouter } from './routes/current-user.js';
+import { signinRouter } from './routes/signin.js';
+import { signoutRouter } from './routes/signout.js';
+import { signupRouter } from './routes/signup.js';
+import { errorHandler } from './middlewares/error-handler.js';
+import { NotFoundError } from './errors/not-found-error.js';
+import cookieSession from 'cookie-session';
+
+import mongoose from 'mongoose';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(cookieSession({
+    signed: false,
+    secure: true
+}));
+
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+
+const start = async () => { 
+    if (!process.env.JWT_KEY) { 
+        throw new Error('JWT_KEY must be defined');
+    }
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+        console.log('Connected to MongoDB');
+    }
+    catch (err) {
+        console.error(err);
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!!!')
+    });
+}
+
+start();
