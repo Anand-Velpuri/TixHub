a09b26f6af9111716c7622f46048d8897a5e0692
Anand-Velpuri <129038384+Anand-Velpuri@users.noreply.github.com>
2026-07-16 11:59:34 +0530

    Handled Validation Errors and updated to turbopack instead of webpack

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    webpackDevMiddleware: config => {
-        config.watchOptions.poll = 300;
-        return config;
-    }
+  allowedDevOrigins: ['ticketing.dev'],
+  turbopack: {},
 };
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,13 +4,21 @@ import axios from 'axios';
 export default () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
 
     const onSubmit = async (event) => {
         event.preventDefault();
 
-        const response = await axios.post('/api/users/signup', { email, password });
+        try { 
 
-        console.log(response.data);
+            const response = await axios.post('/api/users/signup', { email, password });
+    
+            console.log(response.data);
+        } catch (err) {
+            console.log(err.response.data);
+            setErrors(err.response.data.errors);
+        }
+
     };
 
     return (
@@ -25,7 +33,14 @@ export default () => {
                 <label>Password</label>
                 <input value={password} onChange={ e => setPassword(e.target.value)} className="form-control" type="password" />
             </div>
-
+            {errors.length > 0 && (
+                <div className="alert alert-danger">
+                    <h4>Ooops...</h4>
+                    <ul className="my-0">
+                        {errors.map(err => <li key={err.message} className="list-group-item">{err.message}</li>)}
+                    </ul>
+                </div>
+            )}
             <button className="btn btn-primary">Sign Up</button>
         </form>
     );
