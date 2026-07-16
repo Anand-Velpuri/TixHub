04b952c2d10d58617ad397b7fdddb27ec17343bf
Anand-Velpuri <129038384+Anand-Velpuri@users.noreply.github.com>
2026-07-16 15:47:16 +0530

    Added CrossNamespace Service Communication to resolve the SSR issue with get initial props

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,28 @@
-const Landing = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+    console.log(currentUser);
     return <h1>Landing Page</h1>
 };
 
-export default Landing;
+LandingPage.getInitialProps = async ({ req }) => {
+    if (typeof window === 'undefined') {
+        // We are on the server
+        // requests should be made to http://SERVICENAME.NAMESPACE.svc.cluster.local
+        const { data } = await axios.get(
+            "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+            {
+                headers: req.headers
+            }
+        );
+        return data;
+    } else { 
+        // we are on the browser
+        // requests can be made with a base url of ''
+        const { data } = await axios.get('/api/users/currentuser');
+        return data;
+    }
+    return {};
+};
+
+export default LandingPage;
